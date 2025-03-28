bea51faf73bc3077511ea8d4c3e67946425de541
Froillan Kim B. Edem <112741319+Froillan123@users.noreply.github.com>
2025-03-28 22:12:49 +0800

    [Edem] Landing Pages Links Added Contents and Design

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -31,6 +31,18 @@ public class HomeController : Controller
     {
         return View();
     }
+    public IActionResult About()
+    {
+        return View();
+    }
+    public IActionResult Services()
+    {
+        return View();
+    }
+    public IActionResult Contact()
+    {
+        return View();
+    }
 
     public async Task<IActionResult> LoginTask(string? Username, string Password)
     {
