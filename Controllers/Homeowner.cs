86cef3403b0dcab8beb5876dd465d7f49d63fa7f
Froillan Kim B. Edem <112741319+Froillan123@users.noreply.github.com>
2025-03-28 13:58:50 +0800

    test

--- Controllers/Homeowner.cs
+++ Controllers/Homeowner.cs
@@ -1,4 +1,4 @@
-﻿using HomeOwner.Data;
+﻿﻿using HomeOwner.Data;
 using HomeOwner.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -25,39 +25,37 @@ namespace HomeOwner.Controllers
            ViewBag.fullname = HttpContext.Session.GetString("firstname")+ " " + HttpContext.Session.GetString("lastname");
             
         }
-        public IActionResult UpdateProfile(User model)
+        public  void UpdateProfile(User model)
         {
             try
             {
-                // Find the user in the database - MATCH THE PROPERTY NAMES
-                var existingUser = _context.User.FirstOrDefault(u => u.user_id == model.user_id);
+                // Find the user in the database
+                var existingUser =  _context.User.FirstOrDefault(u => u.username == model.username);
 
                 if (existingUser == null)
                 {
-                    return NotFound(); // Return 404 if user is not found
+                  
                 }
 
-                // Update only the modified fields - USE THE CORRECT PROPERTY NAMES
-                existingUser.username = model.username;
-                existingUser.email = model.email;
-                existingUser.user_password = model.user_password; // Consider hashing this!
-                
-                // Update other fields if needed
-                existingUser.firstname = model.firstname;
-                existingUser.lastname = model.lastname;
-                existingUser.address = model.address;
-                existingUser.contact_no = model.contact_no;
+                // Update only the modified fields
+         
+                existingUser.firstname = model?.firstname;
+                existingUser.lastname = model?.lastname;
+                existingUser.email = model?.email;
+                existingUser.address = model?.address;
+                existingUser.contact_no = model?.contact_no;
+           
 
                 _context.User.Update(existingUser);
                 _context.SaveChanges();
 
                 ViewBag.Message = "Profile updated successfully!";
-                return View(existingUser);
+               
             }
             catch (Exception ex)
             {
                 ViewBag.Error = "An error occurred while updating the profile.";
-                return View();
+               
             }
         }
 
